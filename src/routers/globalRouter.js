1613064bb75c2112c8c8080cef0b2476dcf4b1df
hyeongwookimdev <hyeongwookim.dev@gmail.com>
2022-08-17 19:59:11 +0900

    Create Video.js, init.js
    Create Schema, Connect upload controller to data base.

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,14 +1,10 @@
 import express from "express";
 import { join, login } from "../controllers/userController";
-import { trending, search } from "../controllers/videoController";
-/*
-export default는 이름을 바꿀 수 있지만
-각각의 변수를 export할 경우 실제 이름을 그대로 써야한다.
-*/
+import { home, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", trending);
+globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 
