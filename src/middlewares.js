ecc3f40696ece9764e723d6f674a81917af02638
hyeongwookimdev <hyeongwookim.dev@gmail.com>
2022-08-25 16:02:40 +0900

    Implement Edit Profile

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,22 @@
 export const localsMiddleware = (req, res, next) => {
-  res.locals.loggedIn = Boolean(req.session.loggedIn); //로그인 여부를 로컬에 저장,
+  res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
-  res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session.user || {};
   next();
+};
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
 };
