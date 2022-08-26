6dda70a14002c2e42ba83c6767f72c865549cd2e
hyeongwookimdev <hyeongwookim.dev@gmail.com>
2022-08-26 23:14:51 +0900

    Implement User Profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -135,10 +135,10 @@ export const finishGithubLogin = async (req, res) => {
 
 export const logout = (req, res) => {
   req.session.destroy();
-  res.redirect("/");
+  return res.redirect("/");
 };
 export const getEdit = (req, res) => {
-  res.render("edit-profile", { pageTitle: "Edit Profile" });
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
 };
 
 export const postEdit = async (req, res) => {
@@ -197,4 +197,14 @@ export const postChangePassword = async (req, res) => {
   return res.redirect("/users/logout");
 };
 
-export const see = (req, res) => res.send("See User");
+export const see = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+  if (!user) {
+    return res.status(404).render("404", { pageTitle: "User not found" });
+  }
+  return res.render("users/profile", {
+    pageTitle: user.name,
+    user,
+  });
+};
