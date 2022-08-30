d25ac2263d8708c0635667013262620575507a28
hyeongwookimdev <hyeongwookim.dev@gmail.com>
2022-08-30 11:38:39 +0900

    Update style

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -200,8 +200,13 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id).populate("videos");
-  console.log(user);
+  const user = await User.findById(id).populate({
+    path: "videos", // 가장 먼저 populate 하고 싶은 것
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found" });
   }
