52897c1e35ca4c5266afe21f9c922374845d62f2
hyeongwookimdev <hyeongwookim.dev@gmail.com>
2022-09-12 02:36:01 +0900

    fix: comment  error9

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const commentSchema = new mongoose.Schema({
   text: { type: String, required: true },
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-  username: { type: String, required: true, unique: true },
+  username: { type: String },
   video: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Video" },
   createdAt: { type: Date, required: true, default: Date.now },
 });
